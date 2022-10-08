7a3f9576acf30e9843b0c6f93de4b641c8d0a08e
luke <luke@prescryptive.com>
2022-10-08 11:21:51 -0700

    Add FAQ

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,27 +61,63 @@ const Home: NextPage = () => {
       <PoolBalancesProvider>
         <PoolStats />
       </PoolBalancesProvider>
-      
+
       <Box textAlign="center" width="750px">
         <Heading>FAQ</Heading>
 
-        <Accordion>
+        <Accordion textAlign="left">
           <AccordionItem>
             <AccordionButton>
-              <Box flex="1" textAlign="left">What is this?</Box>
+              <Box flex="1">What is this?</Box>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
-              Burn Koin is the first burn pool developed for Koinos.
+              Burn Koin is the first burn pool developed for <Link href="https://koinos.io" target="_blank">Koinos</Link>. &quot;Burn pool&quot; is just the name we use for mining pools on Koinos Proof of Burn.
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem>
             <AccordionButton>
-              <Box flex="1" textAlign="left">Who are you?</Box>
+              <Box flex="1">How does Proof of Burn work?</Box>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
-              Luke and Kui.
+              Proof of Burn is described in <Link href="https://koinos.io/unified-whitepaper" target="_blank">the Koinos whitepaper</Link>.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionButton>
+              <Box flex="1">Are there any fees?</Box>
+              <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel>
+              Yes. The pool takes 5% of profits to cover operating costs. Your initial deposit amount is never touched.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionButton>
+              <Box flex="1">Can I run my own node?</Box>
+              <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel>
+              Yes! In fact, you probably should. Koinos mining is very accessible. You might even be able to use a computer you already have lying around which would make your costs effectively zero. You can find instructions in <Link href="https://docs.koinos.io" target="_blank">the official Koinos documentation</Link>.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionButton>
+              <Box flex="1">Who are you?</Box>
+              <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel>
+              We are Luke Willis and Kui He. Two USA-based dudes who have been involved with Koinos since well before mainnet. Luke handles development. Kui handles operations.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionButton>
+              <Box flex="1">Is this open source?</Box>
+              <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel>
+              Yes! You can find the source code for the smart contracts at <Link href="https://github.com/lukemwillis/koinos-burn-pool" target="_blank">github/koinos-burn-pool</Link> and for the website at <Link href="https://github.com/lukemwillis/koinos-burn-pool-ui" target="_blank">github/koinos-burn-pool-ui</Link>
             </AccordionPanel>
           </AccordionItem>
         </Accordion>
