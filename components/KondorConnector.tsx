398f8d0db0edfb408929f89a88ad467b22ab0846
luke <luke@prescryptive.com>
2022-10-10 12:15:24 -0700

    fix redirect on connect

--- components/KondorConnector.tsx
+++ components/KondorConnector.tsx
@@ -4,29 +4,36 @@ import { useAccount } from "../context/AccountProvider";
 interface ConnectorProps {
   onConnect?: () => void;
   size?: string;
+  connectedVariant?: JSX.Element;
 }
 
-export default function KondorConnector({ onConnect, size = "md" }: ConnectorProps) {
+export default function KondorConnector({
+  onConnect,
+  size = "md",
+  connectedVariant,
+}: ConnectorProps) {
   const { account, isConnecting, connect } = useAccount();
 
   const connectCallback = async () => {
     await connect();
-    if (account && onConnect) {
+    if (onConnect) {
       onConnect();
     }
-  }
+  };
 
   return account ? (
-    <Button
-      onClick={connectCallback}
-      variant="outline"
-      isLoading={isConnecting}
-      minWidth="unset"
-      fontWeight="normal"
-      size={size}
-    >
-      Connected as {account}
-    </Button>
+    connectedVariant || (
+      <Button
+        onClick={connectCallback}
+        variant="outline"
+        isLoading={isConnecting}
+        minWidth="unset"
+        fontWeight="normal"
+        size={size}
+      >
+        Connected as {account}
+      </Button>
+    )
   ) : (
     <Button
       onClick={connectCallback}
--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,12 @@ const Home: NextPage = () => {
   const { account } = useAccount();
 
   return (
-    <Flex direction="column" alignItems="center" padding={{ base: "2em", md: "4em", lg: "8em" }} gap={{ base: "4em", md: "6em" }}>
+    <Flex
+      direction="column"
+      alignItems="center"
+      padding={{ base: "2em", md: "4em", lg: "8em" }}
+      gap={{ base: "4em", md: "6em" }}
+    >
       <Stack direction={{ base: "column", md: "row" }} alignItems="center">
         <Logo size={{ base: "175px", md: "250px" }} />
         <Box textAlign={{ base: "center", md: "left" }}>
@@ -43,83 +48,121 @@ const Home: NextPage = () => {
       </Stack>
 
       <Box>
-        {!account ? (
-          <WalletConnector onConnect={() => router.push("/dashboard")} size="lg" />
-        ) : (
-          <Link href="/dashboard">
-            <Button
-              variant="solid"
-              minWidth="unset"
-              fontWeight="bold"
-              colorScheme="blue"
-              size="lg"
-            >
-              Go to dashboard
-            </Button>
-          </Link>
-        )}
+        <WalletConnector
+          onConnect={() => router.push("/dashboard")}
+          size="lg"
+          connectedVariant={
+            <Link href="/dashboard">
+              <Button
+                variant="solid"
+                minWidth="unset"
+                fontWeight="bold"
+                colorScheme="blue"
+                size="lg"
+              >
+                Go to dashboard
+              </Button>
+            </Link>
+          }
+        />
       </Box>
 
       <PoolBalancesProvider>
         <PoolStats />
       </PoolBalancesProvider>
 
-      <Box textAlign="center" width={{ base: "100vw", md: "750px"}}>
+      <Box textAlign="center" width={{ base: "100vw", md: "750px" }}>
         <Heading>FAQ</Heading>
 
         <Accordion textAlign="left">
           <AccordionItem>
             <AccordionButton>
-              <Box flex="1" textAlign="left">What is this?</Box>
+              <Box flex="1" textAlign="left">
+                What is this?
+              </Box>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
-              Burn Koin is the first burn pool developed for <OutboundLink href="https://koinos.io">Koinos</OutboundLink>. &quot;Burn pool&quot; is just the name we use for mining pools on Koinos Proof of Burn.
+              Burn Koin is the first burn pool developed for{" "}
+              <OutboundLink href="https://koinos.io">Koinos</OutboundLink>.
+              &quot;Burn pool&quot; is just the name we use for mining pools on
+              Koinos Proof of Burn.
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem>
             <AccordionButton>
-              <Box flex="1" textAlign="left">How does Proof of Burn work?</Box>
+              <Box flex="1" textAlign="left">
+                How does Proof of Burn work?
+              </Box>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
-              Proof of Burn is described in <OutboundLink href="https://koinos.io/unified-whitepaper">the Koinos whitepaper</OutboundLink>.
+              Proof of Burn is described in{" "}
+              <OutboundLink href="https://koinos.io/unified-whitepaper">
+                the Koinos whitepaper
+              </OutboundLink>
+              .
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem>
             <AccordionButton>
-              <Box flex="1" textAlign="left">Are there any fees?</Box>
+              <Box flex="1" textAlign="left">
+                Are there any fees?
+              </Box>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
-              Yes. The pool takes 5% of profits to cover operating costs. Your initial deposit amount is never touched.
+              Yes. The pool takes 5% of profits to cover operating costs. Your
+              initial deposit amount is never touched.
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem>
             <AccordionButton>
-              <Box flex="1" textAlign="left">Can I run my own node?</Box>
+              <Box flex="1" textAlign="left">
+                Can I run my own node?
+              </Box>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
-              Yes! In fact, you probably should. Koinos mining is very accessible. You might even be able to use a computer you already have lying around which would make your costs effectively zero. You can find instructions in <OutboundLink href="https://docs.koinos.io">the official Koinos documentation</OutboundLink>.
+              Yes! In fact, you probably should. Koinos mining is very
+              accessible. You might even be able to use a computer you already
+              have lying around which would make your costs effectively zero.
+              You can find instructions in{" "}
+              <OutboundLink href="https://docs.koinos.io">
+                the official Koinos documentation
+              </OutboundLink>
+              .
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem>
             <AccordionButton>
-              <Box flex="1" textAlign="left">Who are you?</Box>
+              <Box flex="1" textAlign="left">
+                Who are you?
+              </Box>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
-              We are Luke Willis and Kui He. Two USA-based dudes who have been involved with Koinos since well before mainnet. Luke handles development. Kui handles operations.
+              We are Luke Willis and Kui He. Two USA-based dudes who have been
+              involved with Koinos since well before mainnet. Luke handles
+              development. Kui handles operations.
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem>
             <AccordionButton>
-              <Box flex="1" textAlign="left">Is this open source?</Box>
+              <Box flex="1" textAlign="left">
+                Is this open source?
+              </Box>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
-              Yes! You can find the source code for the smart contracts at <OutboundLink href="https://github.com/lukemwillis/koinos-burn-pool">github/koinos-burn-pool</OutboundLink> and for the website at <OutboundLink href="https://github.com/lukemwillis/koinos-burn-pool-ui">github/koinos-burn-pool-ui</OutboundLink>
+              Yes! You can find the source code for the smart contracts at{" "}
+              <OutboundLink href="https://github.com/lukemwillis/koinos-burn-pool">
+                github/koinos-burn-pool
+              </OutboundLink>{" "}
+              and for the website at{" "}
+              <OutboundLink href="https://github.com/lukemwillis/koinos-burn-pool-ui">
+                github/koinos-burn-pool-ui
+              </OutboundLink>
             </AccordionPanel>
           </AccordionItem>
         </Accordion>
